479cd756d7a7b280e86e420e3b53d1f0fc505ff6
Zacks <53922473+Zackperez@users.noreply.github.com>
2022-10-27 13:56:59 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ const OPERATORS = ["+", "-", "*", "/", "%", "="];
 const LETRAS = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const ESPACIO = [" "];
 const PALABRAS_RESERVADAS = ["TEXTO","ENTERO","REAL","CAPTURA.TEXTO()"];
-const DELIMITADORES = [";", "(", ")", '""', "''"];
+const DELIMITADORES = [";", "(", ")", '"', "'",":"];
 const isOperator = character => OPERATORS.includes(character);
 const isLetter = character => LETRAS.includes(character);
 const isEmpty = character => ESPACIO.includes(character);
+const isWordReserved = character => PALABRAS_RESERVADAS.includes(character);
+const isDelimiter = character => DELIMITADORES.includes(character);
+
 
 btn_analizar.onclick = function(){
     const texto_area = document.getElementById('entrada_texto').value;
@@ -31,7 +34,15 @@ btn_analizar.onclick = function(){
     
         if (isEmpty(caracter)){
             console.log(caracter+" hay espacio");
-        }
+        };
+
+        if (isWordReserved(caracter)){
+            console.log(caracter+" es una palabra reservada");
+        };
+
+        if (isDelimiter(caracter)){
+            console.log(caracter+" es un delimitador")
+        };
     
         cursor++;
     }
