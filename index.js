156bf6ea015f9ef270c741175d8b00ee59ffd915
Zacks <53922473+Zackperez@users.noreply.github.com>
2022-10-27 00:47:29 -0500

    PRUEBAS

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const texto = "A B C + / 2";
+
 var cursor = 0;
 
-const texto_area = document.getElementById('entrada_texto').value
+const btn_analizar = document.getElementById('btn_analizar');
+const btn_limpiar = document.getElementById('btn_limpiar');
+
+
 
 const OPERATORS = ["+", "-", "*", "/", "%", "="];
-const LETRAS = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const LETRAS = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 const ESPACIO = [" "];
 const PALABRAS_RESERVADAS = ["TEXTO","ENTERO","REAL","CAPTURA.TEXTO()"];
 const DELIMITADORES = [";", "(", ")", '""', "''"];
@@ -12,25 +15,33 @@ const isOperator = character => OPERATORS.includes(character);
 const isLetter = character => LETRAS.includes(character);
 const isEmpty = character => ESPACIO.includes(character);
 
+btn_analizar.onclick = function(){
+    const texto_area = document.getElementById('entrada_texto').value;
 
-
-
-while (cursor < texto_area.length){
-    const caracter = texto_area[cursor];
-
-    if (isOperator(caracter)){
-        console.log(caracter+" es un operador");
-    };
-
-
-    if (isLetter(caracter)){
-        console.log(caracter+" es una letra");
-    };
-
-    if (isEmpty(caracter)){
-        console.log(caracter+" hay espacio")
+    while (cursor < texto_area.length){
+        const caracter = texto_area[cursor];
+    
+        if (isOperator(caracter)){
+            console.log(caracter+" es un operador");
+        };
+    
+        if (isLetter(caracter)){
+            console.log(caracter+" es una letra");
+        };
+    
+        if (isEmpty(caracter)){
+            console.log(caracter+" hay espacio");
+        }
+    
+        cursor++;
     }
 
-    cursor++;
+    cursor = 0;
+
 }
+
+btn_limpiar.addEventListener("click", function(){
+    const texto_area = document.getElementById('entrada_texto').value = "";
+
+})
 
