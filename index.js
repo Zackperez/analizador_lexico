4270a14fefea566d9d2836c60069fb986a90db8b
Zacks <53922473+Zackperez@users.noreply.github.com>
2022-11-09 14:02:17 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,103 +1,162 @@
-document.addEventListener('DOMContentLoaded', function () {
+window.addEventListener("DOMContentLoaded", () => obtenerLexemas());
+const lexico = document.getElementById("datos");
+const sintac = document.getElementById("aSintactico");
+let codigo = document.getElementById("codigo");
+let cCodigo = document.getElementById("container_codigo");
+let lexemas;
+let tokens = [];
 
-    const btn_analizar = document.getElementById('btn_analizar');
-    const btn_limpiar = document.getElementById('btn_limpiar');
+/* obteniendo la informacion de la bd y pasandola a una variable local */
+const obtenerLexemas = async () => {
+  try {
+    const rs = await fetch("./lexemas.json");
+    const data = await rs.json();
+    lexemas = data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    const OPERATORS = ["+", "-", "*", "/", "%", "="];
-    const NUMBER = /^[0-9]+$/
-    const LETRAS = /[a-zA-Z]/
-    const ESPACIO = [" "];
-    const PALABRAS_RESERVADAS = ["TEXTO", "ENTERO", "REAL", "CAPTURA.TEXTO()"];
-    const DELIMITADORES = [";", "(", ")", '"', "'", ":"];
+/* buscar palabra en lexemas */
+function buscar(listaDePalabras) {
+  let subTokens =[];
+  /* recorrer la lista que nos pasan */
+  listaDePalabras.forEach((palabra)=>{
+    /* combrobar si la palabra se encuentra en la bd lexemas */
+    lexemas.some((item)=> item.nombre == palabra)?
+    lexemas.filter((item) => {
+      if (item.nombre == palabra) {
+        subTokens.push(item);
+      }
+    })
+    /* combrobar si la palabra noencontrada no es un string vacio*/
+    : comprobarPalabraNoEncontrada(palabra) !== undefined ?
+      subTokens.push(comprobarPalabraNoEncontrada(palabra)):
+      console.log('es un string vacio')
+      ;
+  });
+  tokens.push(subTokens);
+  console.log(tokens);
+  lexico.innerHTML=``;
+  tokens.forEach(item=>{
+    lexico.innerHTML+=`
+      <tr>
+      <td class='linea text-body-1' colspan='3'>
+      Linea ${tokens.indexOf(item) + 1}
+      </td>
+      </tr>`
+    item.forEach(item=>{
+      lexico.innerHTML+=`
+      <tr>
+      <td class="text-body-1">
+          ${item.nombre}
+      </td>
+      <td class="text-body-1">
+        ${item.tipo}
+      </td>
+      <td class="text-body-1">
+        ${item.codigo}
+      </td>
+    </tr>`;
+    })
+  })
+}
 
-    const isOperator = character => OPERATORS.includes(character);
-    const isNumber = character => NUMBER.test(character);
-    const isLetter = character => LETRAS.test(character);
-    const isEmpty = character => ESPACIO.includes(character);
-    const isWordReserved = character => PALABRAS_RESERVADAS.includes(character);
-    const isDelimiter = character => DELIMITADORES.includes(character);
-
-    btn_analizar.onclick = analizar;
-    btn_limpiar.onclick = limpiar_campos;
-
-    function mostrar(lineas_texto_area){
-        var array = [];
-        array.push(lineas_texto_area);
-        return array
+/*  funcion que compara las palabras no encontradas*/
+function comprobarPalabraNoEncontrada(palabra){
+/*   console.log('No se encontro',palabra); */
+  let temp;
+  /* comprueba si no es un string vacio y no es un numero */
+  if(palabra !== '' && isNaN(palabra)){
+    temp = {
+      nombre: palabra,
+      tipo: "identificador",
+      codigo: "101"
     }
-
-    function analizar() {
-        const lineas_texto_area = document.getElementById('entrada_texto').value.split('\n');
-        const texto_area = document.getElementById('entrada_texto').value;
-        const texto_area2 = document.getElementById('entrada_texto').value.split(/\s/);
-
-
-        for (var i = 0; i < lineas_texto_area.length; i++) {
-            var cursor = 0;
-            console.log("Linea", i + 1, lineas_texto_area[i]);
-            var array = [];
-            while (cursor < texto_area.length) {
-                const caracter = texto_area[cursor];
-
-                if (isOperator(caracter)) {
-                    //console.log("es un operador",caracter);
-                }
-
-                if (isLetter(caracter)) {
-                    let symbol = caracter;
-                    while (isLetter(texto_area[++cursor])) {
-                        symbol += texto_area[cursor];
-                    }
-                }
-
-                if (isNumber(caracter)) {
-                    let number = caracter;
-                    while (isNumber(texto_area[++cursor])) {
-                        number += texto_area[cursor];
-                    }
-                    console.log("Es un numero", number);
-                }
-
-                if (isEmpty(caracter)) {
-                    //console.log("es un espacio",caracter);
-                }
-
-                if (isDelimiter(caracter)) {
-                    //console.log("es un delimitador",caracter);
-                }
-
-                if (caracter == "\n") {
-                    //console.log("Es un salto de línea "+caracter);
-                }
-                cursor++; //Recorre cada caracter
-
-            }
-
-            cursor = 0; // Se reinicia el valor del cursor a 0 para poder terminar de leer los caracteres de la línea
-        }
-
-        analizador_sintactico(lineas_texto_area, texto_area);
+  /* comprueba si no es un string vacio y si es un numero */
+  }else if (palabra !== '' && !isNaN(palabra)){
+    temp = {
+      nombre: palabra,
+      tipo: "número",
+      codigo: "102"
     }
+  }
+  return temp;
+}
 
-    function limpiar_campos() {
-        const texto_area = document.getElementById('entrada_texto').value = "";
-        console.log("area de texto limpiada")
-    }
+/* funcion que divide el texto en bloque de codigo */
+function dividirBloque(cadenas){
+  let bloques = cadenas.split(';\n')
+  console.log(bloques);
+  return bloques;
+}
+/* funcion que separa los casos */
+function comprobarCaso(bloques){
+  let respuestas = [];
+  let respuesta;
+  let i=0;
+  bloques.forEach(item=>{
+    item.startsWith('crear')?
+        item.match(/[a-zA-Z]+([0-9]+)?\s[a-zA-Z]+[;]/)?
+            respuesta =[i+1,'Inicialización de variable']:
+            respuesta =[i+1,`El bloque de codigo ${bloques.indexOf(item)+1} es incorrecta`]
 
-    function analizador_sintactico(lineas_texto_area, texto_area) {
-        let expReg_CrearVariable = /[a-zA-Z]+([0-9]+)?\s[REAL|TEXTO|ENTERO]+[;]/;
-        let expReg_AsignarValorVariable = /[a-zA-Z]+([0-9]+)?\s[=]\s(["][a-zA-Z0-9\s,.-/]+["])?|([a-zA-Z0-9\s,.-/]+)?[;]/;
-        for (var i = 0; i < lineas_texto_area.length; i++) {
-            res = expReg_CrearVariable.test(texto_area);
-            if (expReg_CrearVariable.test(texto_area) == true) {
-                const retorno = {"texto" : texto_area,"tipo": "es una creacion de variable"}
-                console.log(retorno);
-            } else if (expReg_AsignarValorVariable.test(texto_area) == true) {
-                const retorno = {"texto" : texto_area,"tipo": "es una asignacion de variable"}
-                console.log(retorno);
-
-            }
-        }
-    }
-
-});
+    :item.startsWith('asignar')?
+        item.match(/[a-zA-Z]+\s[=]\s(["]([a-zA-Z]+)["])[;]/)?
+        respuesta =[i+1,'Asignación de variable']:
+        respuesta =[i+1,`El bloque de codigo ${bloques.indexOf(item)+1} es incorrecta`]   
+        
+    :respuesta =[i+1,'no reconosco esta sintaxis']
+    respuestas.push(respuesta);
+    console.log(respuestas);
+    i++;
+  })
+  sintac.innerHTML=``;
+  respuestas.forEach(item=>{
+    sintac.innerHTML+=`
+    <p class='text-body-1'>
+    Bloque de codigo ${item[0]}
+    </p>
+    <p class='text-body-1'>
+    ${item[1]}
+    </p>
+    `;
+  })
+}
+/* funcion que divide el texto en lineas */
+function dividirLineas(cadenas){
+  let listDeCadenas = cadenas.split(/\n/g);
+  /*   console.log(listDeCadenas); */
+  return listDeCadenas;
+}
+/* funcion que divide el texto en palabras y luego las busca en la bd lexemas */
+function dividirPalabras(cadenas){
+  /* recorremos las lineas */
+  cadenas.forEach((cadena)=>{
+    /* en cada linea separamos los simbolos*/
+    cadena = cadena.replace(';',' ;');
+    cadena = cadena.replace('=',' =');
+    cadena = cadena.replace('+',' +');
+    cadena = cadena.replace('.',' . ');
+    cadena = cadena.replace('(',' ( ');
+    cadena = cadena.replace(')',' ) ');
+/*     console.log(cadena) */
+  /* separamos cada linea por los espacios */
+  let listaDePalabras = cadena.split(/\s/g);
+  /*     console.log(listaDePalabras); */
+  /* buscamos cada parabra */
+  buscar(listaDePalabras);
+  });
+}
+/* agregando la accion al form */
+cCodigo.addEventListener('submit', event=>{
+  event.preventDefault();
+  tokens=[];
+  dividirPalabras(dividirLineas(codigo.value));
+  comprobarCaso(dividirBloque(codigo.value));
+  /*   dividirLineas() */
+})
+cCodigo.addEventListener('reset', event=>{
+  lexico.innerHTML=``;
+  sintac.innerHTML=``;
+})
